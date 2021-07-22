1987dd2b1f52b7b240509eab5700690ec167cc2c
Vladislav <vlad@grokhotov.ru>
2021-07-22 21:49:46 +0300

    add AlbumVC

---
+++ MobileUpDemo/Album/AlbumVC.swift
@@ -0,0 +1,17 @@
+//
+//  AlbumVC.swift
+//  MobileUpDemo
+//
+//  Created by Vladislav Grokhotov on 22.07.2021.
+//
+
+import UIKit
+
+class AlbumVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
