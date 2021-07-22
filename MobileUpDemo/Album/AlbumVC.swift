241287b1f2e79b86d4f0041793c1f1164a55604f
Vladislav <vlad@grokhotov.ru>
2021-07-22 22:15:45 +0300

    add logout button

--- MobileUpDemo/Album/AlbumVC.swift
+++ MobileUpDemo/Album/AlbumVC.swift
@@ -9,10 +9,30 @@ import UIKit
 
 class AlbumVC: UIViewController {
 
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         title = "Mobile Up Gallery"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            title: "Выход",
+            style: .plain,
+            target: self,
+            action: #selector(logout)
+        )
+        navigationItem.rightBarButtonItem?.tintColor = .black
+    }
+    
+    @objc func logout() {
+        activityIndicator.startAnimating()
+        UserInfoStorage.shared.delete { [weak self] in
+            self?.activityIndicator.stopAnimating()
+            guard let window = UIApplication.shared.windows.first else { return }
+            window.rootViewController = MainVC()
+            window.makeKeyAndVisible()
+            UIView.transition(with: window, duration: 0.7, options: .transitionCrossDissolve, animations: nil, completion: nil)
+        }
     }
 
 }
