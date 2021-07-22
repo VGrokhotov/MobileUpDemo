ccb662b304e0137d4c14dfccc10ecdbbed343818
Vladislav <vlad@grokhotov.ru>
2021-07-22 15:54:23 +0300

    initial commit

---
+++ MobileUpDemo/Main/MainVC.swift
@@ -0,0 +1,18 @@
+//
+//  MainVC.swift
+//  MobileUpDemo
+//
+//  Created by Vladislav Grokhotov on 21.07.2021.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
