6df2cf452e982b2043a5eae639c1d5557338e58e
Vladislav <vlad@grokhotov.ru>
2021-07-24 17:44:20 +0300

    add logic of photo saving

--- MobileUpDemo/PhotoDetails/PhotoDetailsVS.swift
+++ MobileUpDemo/PhotoDetails/PhotoDetailsVS.swift
@@ -29,5 +29,32 @@ class PhotoDetailsVS: UIViewController {
         imageView.contentMode = .scaleAspectFill
         imageView.clipsToBounds = true
         imageView.image = image
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            image: UIImage(systemName: "square.and.arrow.up"),
+            style: .plain,
+            target: self,
+            action: #selector(savePhoto)
+        )
+        navigationItem.rightBarButtonItem?.tintColor = .black
+    }
+    
+    @objc func savePhoto() {
+        let actionSheet = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        let save = UIAlertAction(title: "Save to the gallery", style: .default) { [weak self] _ in
+            guard let self = self else { return }
+            UIImageWriteToSavedPhotosAlbum(self.image, self, #selector(self.saveCompletion), nil)
+        }
+        let cancel = UIAlertAction(title: "Cancel", style: .cancel)
+        
+        actionSheet.addAction(save)
+        actionSheet.addAction(cancel)
+        
+        present(actionSheet, animated: true)
+    }
+    
+    @objc func saveCompletion(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+        print("Save finished!")
+        /// TODO: уведомить пользователя
     }
 }
